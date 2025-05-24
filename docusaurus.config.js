9bc9666c9d444dc2f987f0fcc72076ed4c6d35f7
Stephen Fuqua <stephen@safnet.com>
2025-05-23 22:24:42 -0500

    Temporarily disable failure on bad links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
   organizationName: "stephenfuqua", // Usually your GitHub org/user name.
   projectName: "stephenfuqua.github.io", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn", //"throw",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
